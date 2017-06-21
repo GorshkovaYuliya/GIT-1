ec44d1d0ee2c7fef12c888d3ab8505447826f0af
GorshkovaYuliya <LiyaGoiLiya@gmail.com>
2017-06-21 23:45:22 +0300

    Update Counting.java

--- Counting.java
+++ Counting.java
@@ -1,16 +1,15 @@
 public class Counting {
-    public static void main(String[] args) {
-        System.out.println("This program shows numbers from 0 till 100");
-        for (int i = 0; i <= 100; i ++) {
-            int N = i / 3;
-            if (i % 3 == 0) {
-                System.out.println("3*" + N);
-            }
-            else {
-                System.out.println(i);
-            }
-        }
+  public static void main(String[] args) {
+    System.out.println("This program shows numbers from 0 till 100");
+    for (int i = 0; i <= 100; i ++) {
+      int n = i / 3;
+      if (i % 3 == 0) {
+        System.out.println("3*" + n);
+      } else {
+        System.out.println(i);
+      }
     }
+  }
 }    
   
   
