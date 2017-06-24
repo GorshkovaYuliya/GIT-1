e621a046662e049ff243d124d8d6bc17fb35f7c9
GorshkovaYuliya <LiyaGoiLiya@gmail.com>
2017-06-24 16:39:25 +0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,13 +6,13 @@ public class Fibonacci {
     System.out.println("Enter the  number of the line");
     Scanner in = new Scanner(System.in);
     int CheckFib = in.nextInt();
-	  double FibNumb = 0;
-	  int i = 0;
+    double FibNumb = 0;
+    int i = 0;
     while (FibNumb < CheckFib) {
-	    double OperationSqrt = Math.sqrt(5);
-	    double UseSqrt = (1 + OperationSqrt) / 2;
-	    FibNumb = Math.round(Math.pow(UseSqrt, i) / OperationSqrt);
-	    i++;
+      double OperationSqrt = Math.sqrt(5);
+      double UseSqrt = (1 + OperationSqrt) / 2;
+      FibNumb = Math.round(Math.pow(UseSqrt, i) / OperationSqrt);
+      i++;
     } if (CheckFib == FibNumb && CheckFib >= 0) {
       System.out.println("This number belongs to positive Fibonacci line");
     } else {
