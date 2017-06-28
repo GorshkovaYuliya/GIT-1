cf5f8009c3037c716a9f5685b4f3630c1f5467db
GorshkovaYuliya <LiyaGoiLiya@gmail.com>
2017-06-28 15:15:00 +0300

    I changed Sequence.java

---
+++ SequenceModifided.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class SequenceModifided {
+  public static void main(String[] args) {
+    if (args.length != 0) { 
+      determineSequence(args);
+    } else {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Please, enter sequence and divide numbers by using space");
+      String sequenceString = scanner.nextLine();
+      String[] sequenceStringDivided = sequenceString.split(" ");
+      determineSequence(sequenceStringDivided);
+    }
+  }  
+  /** 
+   * This method determines if the sequence is non-decreasing or not
+   * and puts the answer to the console.
+   */
+  public static void determineSequence(String[] sequenceStringDivided) {
+    try {
+      for (int i = 0; i + 1 <sequenceStringDivided.length ; i++) {
+        if (Integer.parseInt(sequenceStringDivided[i]) > Integer.parseInt(sequenceStringDivided[i + 1])) {
+          System.out.println("Your sequence is not non-decreasing");
+          return;
+        }        
+      }
+    } catch (IllegalArgumentException e) {
+      System.out.println("You entered a wrong symbol");
+    }
+    System.out.println("Your sequence is non-decreasing");
+  }   
+}
