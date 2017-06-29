9f66ca10a5b8fa4097608cab05fc8d0f7e21bc49
GorshkovaYuliya <LiyaGoiLiya@gmail.com>
2017-06-29 17:30:26 +0300

    I fixed my mistakes

--- SequenceModifided.java
+++ SequenceModifided.java
@@ -20,13 +20,13 @@ public class SequenceModifided {
    */
   public static void determineSequence(String[] sequenceStringDivided) {
     try {
-      for (int i = 0; i + 1 <sequenceStringDivided.length ; i++) {
+      for (int i = 0; i + 1 < sequenceStringDivided.length ; i++) {
         if (Integer.parseInt(sequenceStringDivided[i]) > Integer.parseInt(sequenceStringDivided[i + 1])) {
           System.out.println("Your sequence is not non-decreasing");
           return;
         }        
       }
-    } catch (IllegalArgumentException e) {
+    } catch (NumberFormatException e) {
       System.out.println("You entered a wrong symbol");
     }
     System.out.println("Your sequence is non-decreasing");
