98acdfa0b8b25d7c056dac178ab4e6247a2ed267
GorshkovaYuliya <LiyaGoiLiya@gmail.com>
2017-06-30 21:47:38 +0300

    Update ConsoleReading.java

--- SequenceDividedOnClasses/ConsoleReading.java
+++ SequenceDividedOnClasses/ConsoleReading.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
 /**
- * This class suggest user to enter  sequence
+ * This class suggest user to enter  sequence.
  */
 public class ConsoleReading {
 
   /**
    * This method read sequence enered by user and
-   * divided it on separate symbols
+   * divided it on separate symbols.
    *
-   *@return sequenceStringDivided enered and divided sequence
+   *@return sequenceStringDivided entered and divided sequence.
    */
   public String[] readConsole () {
     Scanner scanner = new Scanner(System.in);
