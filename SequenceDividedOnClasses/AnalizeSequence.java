49a9267cf9c7b43906ba563980312d8c7020bff9
GorshkovaYuliya <LiyaGoiLiya@gmail.com>
2017-07-04 15:06:02 +0300

    Update AnalizeSequence.java

--- SequenceDividedOnClasses/AnalizeSequence.java
+++ SequenceDividedOnClasses/AnalizeSequence.java
@@ -2,31 +2,11 @@
  * This class analize your sequence if it is non-decreasing.
  */
 public class AnalizeSequence {
-
-  /**
-   * This method  check entered sequence if all symbols are numbers
-   *
-   * @param sequenceStringDivided checking sequence
-   * @exception NumberFormatException if e puts not numbers
-   * @return false if there is not only numbers
-   * @return true if the sequence consists only of numbers
-   */
-  public boolean checkingSequenceOnNumbers(String[] sequenceStringDivided) {
-    try {
-      for (int i = 0; i < sequenceStringDivided.length - 1; i++) { 
-        Integer.parseInt(sequenceStringDivided[i]);
-      }
-    } catch (NumberFormatException e) {
-      System.out.println("You entered a wrong symbol");
-      return false;
-    }
-    return true;
-  }
   
   /**
    * This method determines if the sequence is non-decreasing or not
    *
-   * @param sequenceStringDivided checking sequence
+   * @param args checking sequence
    * @return false if the sequence is not non-decreasing
    * @return true if the sequence is non-decreasing
    */
