77a0f833af954153327f32ad58296a78c538f7b5
GorshkovaYuliya <LiyaGoiLiya@gmail.com>
2017-07-04 15:48:40 +0300

    Update CheckSequenceRecursive.java

--- SequenceDividedOnClasses/CheckSequenceRecursive.java
+++ SequenceDividedOnClasses/CheckSequenceRecursive.java
@@ -1,17 +1,25 @@
-
+/**
+ * This class is recursion
+ */
 public class CheckSequenceRecursive {
-  public void checkSequenceRecursive(String[] args) {
+
+  /**
+   * This method allows user to check sequence if user
+   * enered it or call for console read method
+   *
+   * @param args checking sequence
+   */
+  public void dissectSequenceRecursive(String[] args) {
     AnalizeSequence sequenceCheck = new AnalizeSequence();
     if (args.length != 0) {
-	    if (sequenceCheck.determineSequence(args) == true) {
-	      System.out.println("Your sequence is non-decreasing");
-	    } else {
-	      System.out.println("Your sequence is not non-decreasing");  
-	    }
+      if (sequenceCheck.determineSequence(args) == true) {
+	System.out.println("Your sequence is non-decreasing");
+      } else {
+	System.out.println("Your sequence is not non-decreasing");  
+      }
     } else {
-	  ConsoleReading consoleRead = new ConsoleReading();
-	  consoleRead.readConsole();
-	 
+      ConsoleReading consoleRead = new ConsoleReading();
+      consoleRead.readConsole(); 
     }
   }
 }
