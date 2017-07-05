3c5fa5cc80944330c10ca11afc89f4892d786bb4
GorshkovaYuliya <LiyaGoiLiya@gmail.com>
2017-07-05 23:51:34 +0300

    Update SequenceModifided.java

--- SequenceDividedOnClasses/SequenceModifided.java
+++ SequenceDividedOnClasses/SequenceModifided.java
@@ -7,25 +7,20 @@
  */
 public class SequenceModifided {
   public static void main(String[] args) {
-    ConsoleReading consoleRead = new ConsoleReading();
-    AnalizeSequence sequenceCheck = new AnalizeSequence();
     String[] sequenceForCheck = args;
+    ConsoleReading consoleRead = new ConsoleReading();
     if (args.length <= 1) {
       sequenceForCheck = consoleRead.readConsole();
     }
-    while(sequenceForCheck.length <= 1) {
-      System.out.println("You do not put enough values");
+    while(sequenceForCheck.length <= 1 || !(sequenceCheck.determineIsSequensCorrect(sequenceForCheck))) {
+      System.out.println("You do not put enough or entered impermissible values");
       sequenceForCheck = consoleRead.readConsole();
     }
-    try {
-      sequenceCheck.determineSequence(sequenceForCheck);
-      if (sequenceCheck.determineSequence(sequenceForCheck) == true) {
-        System.out.println("Your sequence is non-decreasing");
-      } else {
-        System.out.println("Your sequence is not non-decreasing");  
-      }
-    } catch (NumberFormatException e) { 
-      System.out.println("Wrong format of enetered line");
+    AnalizeSequence sequenceCheck = new AnalizeSequence();
+    if (sequenceCheck.determineSequence(sequenceForCheck)) {
+      System.out.println("Your sequence is non-decreasing");
+    } else {
+      System.out.println("Your sequence is not non-decreasing");  
     }
   }
 }
