f3aca80e634dc1e91327b6e9c80464f7049579a3
GorshkovaYuliya <LiyaGoiLiya@gmail.com>
2017-07-12 01:28:12 +0300

    This is the first class work. Person

---
+++ Persons/Personas.java
@@ -0,0 +1,42 @@
+/*
+ * This class contains person's data
+ */
+public class Personas {
+  private String firstName;
+  private String secondName;
+  private String patronomic;
+  private int age;
+  public Personas (String firstName, String secondName, String patronomic, int age) {
+    this.firstName = firstName;
+	this.secondName = secondName;
+	this.patronomic = patronomic;
+	this.age = age;
+  }
+  public void setAge(int age) {
+    this.age = age;
+  }	
+  public String getFirstName() {
+    return firstName;
+  }
+  public String getSecondName() {
+    return secondName;
+  }
+  public String getPatronomic() {
+    return patronomic;
+  }
+  public int getAge() {
+    return age;
+  }
+   
+  /*
+   * This metod check age value on correctness
+   *
+   * @return true if age value is correct, and false otherwise 
+   */
+  public boolean isAgeCorrect(int age) {
+    if ((age < 0) || (age > 150) || (age % 1 != 0)) {
+	  return false;
+	}
+	return true;
+  }
+}
---
+++ Persons/PersonsData.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+/*
+ * This programm formes a list of Person's 
+ * data: firstname, secondname, patronomic,age
+ *
+ * @author Gorshkova Yuliya
+ */
+public class PersonsData {
+  public static void main(String[] args) {
+	   try
+           {
+    System.out.println("Enter amount of data to be fill out");
+	Scanner scanner = new Scanner(System.in);
+	int amountOfData = scanner.nextInt();
+	System.out.println("Fill in the data: ");
+	Personas[] person = new Personas[amountOfData];
+	for (int i = 0; i < amountOfData; i++) {
+	  scanner.nextLine();
+	  System.out.println("Eneter the firstname of the " + (i + 1) + " person:");
+	  String firstNameOfPerson = scanner.nextLine();
+	  System.out.println("Eneter the secondname of the " + (i + 1) + " person:");
+	  String secondNameOfPerson = scanner.nextLine();
+	  System.out.println("Eneter the patronymic of the " + (i + 1) + " person:");
+	  String patronomicOfPerson = scanner.nextLine();
+	  System.out.println("Eneter the age of the " + (i + 1) + " person:");
+	  int ageOfPerson = scanner.nextInt();
+	  person[i] = new Personas(firstNameOfPerson, secondNameOfPerson, patronomicOfPerson,ageOfPerson);
+	  while(!(person[i].isAgeCorrect(ageOfPerson))) {
+	    System.out.println("You entered wrong values, plase enter again:");
+		  ageOfPerson = scanner.nextInt();
+		  person[i].setAge(ageOfPerson);
+	  }
+	}
+	for (int i = 0; i < amountOfData; i++) {
+	  System.out.println("Person number " + (i + 1));
+      System.out.println( person[i].getFirstName() + " " + person[i].getSecondName() + " " + person[i].getPatronomic() + "  " + person[i].getAge());
+    }
+		   } catch (Exception e) {
+			   System.out.println("Wrong entered value");
+			   main(args);
+		   }
+  }
+}
