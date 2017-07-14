9437c41737463b80424144d47315a96f9735409a
GorshkovaYuliya <LiyaGoiLiya@gmail.com>
2017-07-14 15:54:54 +0300

    changed structure

--- Persons/ConsoleRead.java
+++ Persons/ConsoleRead.java
@@ -1,28 +1,13 @@
 import java.util.Scanner;
 /**
  * This class reads the data of the person  from the console
+ *
+ * @return data read from console
  */
 public class ConsoleRead {
-
-  /**
-   * This method reads the  string data of the person  from the console
-   *
-   * @return  string data of person entered from console
-   */
-  public String consoleReadingString() {
+  public String consoleReading(String fieldName) {
     Scanner scanner = new Scanner(System.in);
-	  String dataString = scanner.nextLine();
-	  return dataString;
-  }
-
-  /**
-   * This method reads the  integer data of the person  from the console
-   *
-   * @return  integer data of person entered from console
-   */
-  public int consoleReadingInteger() {
-    Scanner scanner = new Scanner(System.in);
-	  int dataInteger = scanner.nextInt();
-	  return dataInteger;
-  }
+	  System.out.println(fieldName);
+	  return scanner.nextLine();
+  } 
 }
