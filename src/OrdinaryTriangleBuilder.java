c31be1bff55351caf4b1c0fe3964b3f5536decde
GorshkovaYuliya <LiyaGoiLiya@gmail.com>
2017-07-27 12:44:00 +0300

    add overload for ordinary

--- src/OrdinaryTriangleBuilder.java
+++ src/OrdinaryTriangleBuilder.java
@@ -5,24 +5,11 @@ import java.math.BigDecimal;
  */
 public class OrdinaryTriangleBuilder extends TriangleBuilder {
   public OrdinaryTriangleBuilder(TriangleBuilder successor) {
-    super();
     this.successor = successor;
   }
 
-  /**
-   * Tries to build ordinary triangle
-   * @param sides is sides of triangle
-   * @return build ordinary triangle
-   * @throws IllegalTriangleException if triangle can't be build
-   * @throws IllegalTypeTriangleException if triangle is not ordinary
-   */
-  public Triangle buildTriangle(BigDecimal[] sides) throws IllegalTriangleException, IllegalTypeTriangleException {
-    try {
-      return new Triangle(sides);
-    } catch (IllegalTriangleException exc) {
-      if (successor != null) {
-        return successor.buildTriangle(sides);
-      } else throw new IllegalTriangleException();
-    }
+  @Override
+  public Triangle buildSpecialTypeOfTheTriangle(BigDecimal[] sides) throws IllegalTypeTriangleException, IllegalTriangleException {
+    return new Triangle(sides);
   }
 }
