c13a45a6da9ca0319410f2af3956d8a82ef6583a
GorshkovaYuliya <LiyaGoiLiya@gmail.com>
2017-07-27 13:01:15 +0300

    Replaced while in if

--- Persons/Personas.java
+++ Persons/Personas.java
@@ -29,7 +29,7 @@ public class Personas {
    */
   public void setAge(String age) {
     this.age = age;
-    while (!(isAgeCorrect(age))) {
+    if (!(isAgeCorrect(age))) {
       throw new IllegalArgumentException();
     }
   }
