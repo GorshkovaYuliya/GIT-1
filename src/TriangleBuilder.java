69f213294960b96bea8c16498647b68080e4376c
GorshkovaYuliya <LiyaGoiLiya@gmail.com>
2017-07-27 12:42:07 +0300

    Add method

--- src/TriangleBuilder.java
+++ src/TriangleBuilder.java
@@ -1,10 +1,35 @@
 import java.math.BigDecimal;
-
 /**
- * Created by User on 21.07.2017.
+ * This class build triangle
  */
 abstract class TriangleBuilder {
   protected TriangleBuilder successor;
 
-  public abstract Triangle buildTriangle(BigDecimal[] sides) throws IllegalTriangleException, IllegalTypeTriangleException;
+  public TriangleBuilder(){
+    this.successor = successor;
+  }
+  
+  /**
+   * Tries to build triangle.
+   * @param sides is sides of triangle
+   * @return build triangle
+   * @throws IllegalTriangleException if triangle can't be build
+   * @throws IllegalTypeTriangleException if triangle type is invalid
+   */
+  public Triangle buildTriangle(BigDecimal[] sides) throws IllegalTriangleException, IllegalTypeTriangleException{
+    try {
+      return  buildSpecialTypeOfTheTriangle(sides);
+    } catch (IllegalTypeTriangleException e) {
+      if (successor != null) {
+        return successor.buildTriangle(sides);
+      }
+      throw new  IllegalTriangleException ( );
+    }
+  }
+  
+  /**
+   * Tries to build special type of triangle.
+   * @param sides is sides of triangle
+   */
+  public abstract Triangle buildSpecialTypeOfTheTriangle(BigDecimal[] sides) throws IllegalTypeTriangleException, IllegalTriangleException;
 }
