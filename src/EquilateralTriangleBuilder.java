be0506fdd35e5cd71084d39d41949948893cd60c
GorshkovaYuliya <LiyaGoiLiya@gmail.com>
2017-07-27 12:47:58 +0300

    add overload for equilateral

--- src/EquilateralTriangleBuilder.java
+++ src/EquilateralTriangleBuilder.java
@@ -11,25 +11,11 @@ public class EquilateralTriangleBuilder extends TriangleBuilder {
    * @param successor points to the next type of triangle
    */
   public EquilateralTriangleBuilder(TriangleBuilder successor) {
-    super();
     this.successor = successor;
   }
 
-  /**
-   * Tries to build equilateral triangle
-   * @param sides is sides of triangle
-   * @return build equilateral triangle
-   * @throws IllegalTriangleException if triangle can't be build
-   * @throws IllegalTypeTriangleException if triangle is not equilateral
-   */
-  public Triangle buildTriangle(BigDecimal[] sides) throws IllegalTriangleException, IllegalTypeTriangleException {
-    try {
-      return new EquilateralTriangle(sides);
-    } catch (IllegalTypeTriangleException e) {
-      if (successor != null) {
-        return successor.buildTriangle(sides);
-      }
-      throw new  IllegalTriangleException ( );
-    }
+  @Override
+  public Triangle buildSpecialTypeOfTheTriangle(BigDecimal[] sides) throws IllegalTypeTriangleException, IllegalTriangleException {
+    return new EquilateralTriangle(sides);
   }
 }
