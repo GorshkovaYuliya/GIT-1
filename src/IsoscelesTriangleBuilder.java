f20903d407b7c33e716aabd34583595dccff0d48
GorshkovaYuliya <LiyaGoiLiya@gmail.com>
2017-07-27 12:45:34 +0300

    add overload for isosceles

--- src/IsoscelesTriangleBuilder.java
+++ src/IsoscelesTriangleBuilder.java
@@ -10,25 +10,11 @@ public class IsoscelesTriangleBuilder extends TriangleBuilder {
    * @param successor points to the next type of triangle
    */
   public IsoscelesTriangleBuilder(TriangleBuilder successor) {
-    super();
     this.successor = successor;
   }
 
-  /**
-   * Tries to build isosceles triangle
-   * @param sides is sides of triangle
-   * @return built isosceles triangle
-   * @throws IllegalTriangleException if triangle can't be build
-   * @throws IllegalTypeTriangleException if triangle is not isosceles
-   */
-  public Triangle buildTriangle(BigDecimal[] sides) throws IllegalTriangleException, IllegalTypeTriangleException {
-    try {
-      return new IsoscelesTriangle(sides);
-    } catch (IllegalTypeTriangleException e) {
-      if (successor != null) {
-        return successor.buildTriangle(sides);
-      }
-      throw new  IllegalTriangleException ( );
-    }
+  @Override
+  public Triangle buildSpecialTypeOfTheTriangle(BigDecimal[] sides) throws IllegalTypeTriangleException, IllegalTriangleException {
+    return new IsoscelesTriangle(sides);
   }
 }
